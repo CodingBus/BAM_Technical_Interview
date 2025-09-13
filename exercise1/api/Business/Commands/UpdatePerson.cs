4663a6d6d6e0790fa1c0238de0f1ed3094ef2992
CodingBus <devindperez@gmail.com>
2025-09-13 09:43:10 -0700

    scaffold mediatr components for PUT /person

---
+++ exercise1/api/Business/Commands/UpdatePerson.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using MediatR.Pipeline;
+using Microsoft.EntityFrameworkCore;
+using StargateAPI.Business.Data;
+using StargateAPI.Controllers;
+
+namespace StargateAPI.Business.Commands
+{
+    public class UpdatePerson : IRequest<UpdatePersonResult>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string NewName { get; set; } = string.Empty;
+    }
+
+    public class UpdatePersonPreProcessor : IRequestPreProcessor<UpdatePerson>
+    {
+        private readonly StargateContext _context;
+        public UpdatePersonPreProcessor(StargateContext context)
+        {
+            _context = context;
+        }
+        public async Task Process(UpdatePerson request, CancellationToken cancellationToken)
+        {
+            
+        }
+    }
+
+    public class UpdatePersonHandler : IRequestHandler<UpdatePerson, UpdatePersonResult>
+    {
+        private readonly StargateContext _context;
+
+        public UpdatePersonHandler(StargateContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UpdatePersonResult> Handle(UpdatePerson request, CancellationToken cancellationToken)
+        {
+                return new UpdatePersonResult()
+                {
+                    Id = 0
+                };
+          
+        }
+    }
+
+    public class UpdatePersonResult : BaseResponse
+    {
+        public int Id { get; set; }
+    }
+}
